8be798b2f748678b5c5e7ac32bc442a15cf4d36d
Renato Viegas <renato.viegas@hotmail.com>
2019-03-27 10:15:50 -0300

    Ajustes

--- desafio-fullstack/src/main/java/com/involves/selecao/service/ProcessadorAlertas.java
+++ desafio-fullstack/src/main/java/com/involves/selecao/service/ProcessadorAlertas.java
@@ -1,14 +1,19 @@
 package com.involves.selecao.service;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonToken;
 import com.involves.selecao.alerta.Alerta;
 import com.involves.selecao.alerta.Pesquisa;
 import com.involves.selecao.alerta.Resposta;
@@ -19,63 +24,71 @@ public class ProcessadorAlertas {
 
 	@Autowired
 	private AlertaGateway gateway;
-	
+
 	public void processa() throws IOException {
 		URL url = new URL("http://selecao-involves.agilepromoter.com/pesquisas");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
-		
-		BufferedReader in = new BufferedReader(
-		  new InputStreamReader(con.getInputStream(), "UTF-8"));
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
 		String inputLine;
 		StringBuffer content = new StringBuffer();
-		
+
 		while ((inputLine = in.readLine()) != null) {
-		    content.append(inputLine);
+			content.append(inputLine);
 		}
 		in.close();
 
 		Gson gson = new Gson();
-		Pesquisa[] ps =  gson.fromJson(content.toString(), Pesquisa[].class);
-		for (int i = 0; i < ps.length; i++){
-			for (int j = 0; j < ps[i].getRespostas().size(); j++){
-				Resposta resposta = ps[i].getRespostas().get(j);
-				if (resposta.getPergunta().equals("Qual a situaÃ§Ã£o do produto?")) {
-					if(resposta.getResposta().equals("Produto ausente na gondola")){
-					    Alerta alerta = new Alerta();
-					    alerta.setPontoDeVenda(ps[i].getPonto_de_venda());
-					    alerta.setDescricao("Ruptura detectada!");
-					    alerta.setProduto(ps[i].getProduto());
-					    alerta.setFlTipo(1);
-					    gateway.salvar(alerta);
-					}
-				} else if(resposta.getPergunta().equals("Qual o preÃ§o do produto?")) {
-					int precoColetado = Integer.parseInt(resposta.getResposta());
-					int precoEstipulado = Integer.parseInt(ps[i].getPreco_estipulado());
-					if(precoColetado > precoEstipulado){
-					    Alerta alerta = new Alerta();
-					    int margem = precoEstipulado - Integer.parseInt(resposta.getResposta());
-					    alerta.setMargem(margem);
-					    alerta.setDescricao("PreÃ§o acima do estipulado!");
-					    alerta.setProduto(ps[i].getProduto());
-					    alerta.setPontoDeVenda(ps[i].getPonto_de_venda());
-					    alerta.setFlTipo(2);
-					    gateway.salvar(alerta);
-					} else if(precoColetado < precoEstipulado){
+		System.out.println(content.toString());
+		Pesquisa[] ps = gson.fromJson(content.toString(), Pesquisa[].class);
+		// Pesquisa[] ps = gson.fromJson(content.toString(), Pesquisa[].class);
+
+		List<Pesquisa> pesquisas = new ArrayList<Pesquisa>();
+		for (int i = 0; i < ps.length; i++) {
+			pesquisas.add(ps[i]);
+		}		
+		
+		pesquisas.forEach(pesquisa -> {
+			List<Resposta> respostas = pesquisa.getRespostas();
+
+			respostas.forEach(resposta -> {
+				if (resposta.getPergunta().equals("Qual a situação do produto?")) {
+					if (resposta.getResposta().equals("Produto ausente na gondola")) {
 						Alerta alerta = new Alerta();
-					    int margem = precoEstipulado - Integer.parseInt(resposta.getResposta());
-					    alerta.setMargem(margem);
-					    alerta.setDescricao("PreÃ§o abaixo do estipulado!");
-					    alerta.setProduto(ps[i].getProduto());
-					    alerta.setPontoDeVenda(ps[i].getPonto_de_venda());
-					    alerta.setFlTipo(3);
-					    gateway.salvar(alerta);
+						alerta.setPontoDeVenda(pesquisa.getPonto_de_venda());
+						alerta.setDescricao("Ruptura detectada!");
+						alerta.setProduto(pesquisa.getProduto());
+						alerta.setFlTipo(1);
+						gateway.salvar(alerta);
+					}
+				} else if (resposta.getPergunta().equals("Qual o preço do produto?")) {
+					int precoColetado = Integer.parseInt(resposta.getResposta());
+					int precoEstipulado = Integer.parseInt(pesquisa.getPreco_estipulado());
+					if (precoColetado > precoEstipulado) {
+						Alerta alerta = new Alerta();
+						int margem = precoEstipulado - Integer.parseInt(resposta.getResposta());
+						alerta.setMargem(margem);
+						alerta.setDescricao("Preço acima do estipulado!");
+						alerta.setProduto(pesquisa.getProduto());
+						alerta.setPontoDeVenda(pesquisa.getPonto_de_venda());
+						alerta.setFlTipo(2);
+						gateway.salvar(alerta);
+					} else if (precoColetado < precoEstipulado) {
+						Alerta alerta = new Alerta();
+						int margem = precoEstipulado - Integer.parseInt(resposta.getResposta());
+						alerta.setMargem(margem);
+						alerta.setDescricao("Preço abaixo do estipulado!");
+						alerta.setProduto(pesquisa.getProduto());
+						alerta.setPontoDeVenda(pesquisa.getPonto_de_venda());
+						alerta.setFlTipo(3);
+						gateway.salvar(alerta);
 					}
 				} else {
-					System.out.println("Alerta ainda nÃ£o implementado!");
+					System.out.println("Alerta ainda não implementado!");
 				}
-			} 
-		}
+			});
+		});
+
 	}
 }
-
